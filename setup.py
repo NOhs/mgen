cc26bd4af3a2e1401755f8e20a91c86752acc7e7
Nicholas Ohs <nicholas.ohs@outlook.com>
2018-01-22 00:32:27 +0100

    Added basic setup.py and added to .gitignore.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='mgen',
+    version='0.1',
+    description='Matrix generation functions',
+    author='NOhs',
+    packages=['mgen'],
+    long_description=long_description,
+    install_requires=['numpy'],
+    license='BSD',
+    url='https://github.com/NOhs/mgen',
+    keywords=['matrix', 'rotations', 'Euler angles'])
