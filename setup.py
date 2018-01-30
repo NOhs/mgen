6180a11fa5046f76c95bf285437929beec82f7c3
NOhs <nicholas.ohs@outlook.com>
2018-01-30 22:17:18 +0100

    Replaced custom setup stuff with pbr.

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-
-with open('README.rst') as readme:
-    long_description = readme.read()
-
-setup(
-    name='mgen',
-    version={VERSION},
-    description='Matrix generation functions',
-    author='NOhs',
-    packages=['mgen'],
-    long_description=long_description,
-    install_requires=['numpy'],
-    license='BSD',
-    url='https://github.com/NOhs/mgen',
-    keywords=['matrix', 'rotations', 'Euler angles'])
+import setuptools
+ 
+setuptools.setup(setup_requires=['pbr'], pbr=True)
