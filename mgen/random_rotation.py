a1f99fd4be604948dcb00c8ab18af26830efeea9
beneit <benedikteiteneuer@gmx-topmail.de>
2018-02-09 20:48:54 +0100

    Fixed weird syntax

--- mgen/random_rotation.py
+++ mgen/random_rotation.py
@@ -12,7 +12,7 @@ def random_rotation(n):
 	:rtype: a nxn :any:`numpy.ndarray`
 	'''
     if n < 2:
-        raise ValueError('n must be greater than 1 but is ' + n '.')
+        raise ValueError('n must be greater than 1 but is ' + n + '.')
     
     a = np.random.randn(n,n)
     
