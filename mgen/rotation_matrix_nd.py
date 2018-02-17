834f6549181d5925e0829b45fe3a71f09415df4d
beneit <benedikteiteneuer@gmx-topmail.de>
2018-02-17 02:18:23 +0100

    added random rotations
    added random rotations

--- mgen/rotation_matrix_nd.py
+++ mgen/rotation_matrix_nd.py
@@ -66,3 +66,39 @@ def rotation_from_angle_and_plane(angle, vector1, vector2, abs_tolerance=1e-10):
     W = np.outer(vector1, vector2) - np.outer(vector2, vector1)
 
     return np.eye(len(vector1)) + (math.cos(angle) - 1)*V - math.sin(angle)*W
+
+def random_matrix(n):
+    '''
+    Generate a nxn random matrix.
+    The distribution of rotations is uniform on the n-sphere.
+    The random matrix is from the O(n) group (not SO(n)) and uses the
+    gram-schmidt algorithm to orthogonalize the random matrix,
+    see http://www.ams.org/notices/200511/what-is.pdf
+    If a rotation from SO(n) is needed, look for:
+    "A statistical model for random rotations" doi:10.1016/j.jmva.2005.03.009
+    
+    :param n: dimension of space in which the rotation operates
+    :type n: int
+    :returns: rotation matrix
+    :rtype: a nxn :any:`numpy.ndarray`
+    '''
+    if n < 2:
+        raise ValueError('n must be greater than 1 but is ' + str(n) + '.')
+    
+    a = np.random.randn(n,n)
+    
+    a[0] = a[0]/np.linalg.norm(a[0])
+    
+    max_iter = n*(n-1)//2
+    base = 0
+    mod = base + 1
+    for i in range(0,max_iter):
+        a[mod] = a[mod] - a[base]*np.dot(a[mod],a[base])
+        a[mod] = a[mod]/np.linalg.norm(a[mod])
+        
+        if mod < n-1:
+            mod += 1
+        else:
+            base += 1
+            mod = base + 1
+    return a
--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -22,6 +22,7 @@ from mgen import rotation_around_z
 from mgen import rotation_around_axis
 from mgen import rotation_from_angle_and_plane
 from mgen import rotation_from_angle
+from mgen import random_matrix
 
 def is_close(m1, m2):
     np.testing.assert_allclose(m1, m2, atol=1.e-7)
@@ -161,3 +162,14 @@ class TestRotations(TestCase):
             rotation_from_angle_and_plane(0., (0,0), (1,0))
         with self.assertRaises(ValueError):
             rotation_from_angle_and_plane(0., (1,0), (0,0))
+            
+    def test_random_matrix(self):
+        for x in range(2,7):
+            vecx = np.zeros(x, dtype=np.float64)
+            vecx[0] = 1.0
+            m = random_matrix(x)
+            rotx = np.matmul(m,vecx)
+            lengthx = np.sum(rotx*rotx)
+            self.assertAlmostEqual(lengthx, 1.0)
+        with self.assertRaises(ValueError):
+            random_matrix(1)
